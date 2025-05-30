7da34948b8a5dc54cd96cccf71c2a3192d47a3bf
José Leandro <joseleand06@gmail.com>
2025-05-29 22:42:04 -0300

    Etapa 1: Criação do projeto com Springboot, dependencias e conexão com banco de dados MySQL

---
+++ src/main/java/com/estudos/springboot_laudos_medicos/SpringbootLaudosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.springboot_laudos_medicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLaudosMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLaudosMedicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/springboot_laudos_medicos/SpringbootLaudosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.springboot_laudos_medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLaudosMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
